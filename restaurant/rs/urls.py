44f7b96fae37dfa80738a894002154cc8b491389
saim75 <kazisaim98@gmail.com>
2023-03-03 22:42:51 +0600

    sample commit

---
+++ restaurant/rs/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path,include
+from rs import views
+
+urlpatterns = [
+    path('', views.index,name='home'),
+    path('home', views.home, name='home'),
+    
+    path('menu', views.menu,name='menu'),
+    path('reservations', views.reservations,name='reservations'),
+    path('contact', views.contact,name='contact'),
+    path('gallery', views.gallery,name='gallery'),
+    path('signup', views.handleSignup, name='handlesSignup'),
+    path('login', views.handlelogin, name='handleslogin'),
+    path('logout', views.handlelogout, name='handleslogout'),
+
+]
